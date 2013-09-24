a5fd9364e27b3bf111007862f79c39cd0d7b2180
enkunkun <enkunkun@gmail.com>
2013-09-25 02:00:09 +0900

    cleanup

--- src/net/enkun/javatter/history/HistoryController.java
+++ src/net/enkun/javatter/history/HistoryController.java
@@ -4,7 +4,6 @@ import twitter4j.DirectMessage;
 import twitter4j.Status;
 import twitter4j.TwitterException;
 import twitter4j.User;
-import twitter4j.UserList;
 
 import com.orekyuu.javatter.account.TwitterManager;
 import com.orekyuu.javatter.controller.UserStreamController;
@@ -31,10 +30,8 @@ public class HistoryController extends UserStreamController {
 	//被リツイート
 	@Override
 	public void onStatus(Status status) {
-		if (status.isRetweet()) {
-			if (isMe(status.getRetweetedStatus().getUser())) {
-				model.onRetweet(status.getUser(), status.getRetweetedStatus());
-			}
+		if (status.isRetweet() && isMe(status.getRetweetedStatus().getUser())) {
+			model.onRetweet(status.getUser(), status.getRetweetedStatus());
 		}
 	}
 
@@ -60,50 +57,11 @@ public class HistoryController extends UserStreamController {
 		}
 	}
 	
+	//TODO
 	@Override
-	public void onDirectMessage(DirectMessage directMessage) {
-		// TODO 検証
-	}
-	
-	@Override
-	public void onException(Exception ex) {
-		ex.printStackTrace();
-	}
-
-	@Override
-	public void onDeletionNotice(long directMessageId, long userId) {}
+	public void onDirectMessage(DirectMessage directMessage) {}
 	
 	@Override
 	public void onBlock(User source, User blockedUser) {}
-	
-	@Override
-	public void onFriendList(long[] friendIds) {}
-
-	@Override
-	public void onUnblock(User source, User unblockedUser) {}
-	
-	@Override
-	public void onUserListCreation(User listOwner, UserList list) {}
-
-	@Override
-	public void onUserListDeletion(User listOwner, UserList list) {}
-
-	@Override
-	public void onUserListMemberAddition(User addedMember, User listOwner, UserList list) {}
-
-	@Override
-	public void onUserListMemberDeletion(User deletedMember, User listOwner, UserList list) {}
-
-	@Override
-	public void onUserListSubscription(User subscriber, User listOwner, UserList list) {}
-
-	@Override
-	public void onUserListUnsubscription(User subscriber, User listOwner, UserList list) {}
-
-	@Override
-	public void onUserListUpdate(User listOwner, UserList list) {}
-
-	@Override
-	public void onUserProfileUpdate(User updatedUser) {}
 
 }
